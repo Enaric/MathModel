266d6d2b1ce8b1ed07bb04edf748a57c11bddfd0
xujiangcheng <mf1832197@smail.nju.edu.cn>
2019-09-20 22:12:17 +0800

    extract fragments

---
+++ Code/extract_fragments.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import datetime
+
+# read csv
+# d = pd.read_csv('processed_data/file1.csv')
+# d = pd.read_csv('processed_data/file2.csv')
+d = pd.read_csv('processed_data/file3.csv')
+print(d.shape)
+
+i = 0
+spl = []
+while i < d.shape[0] - 1:
+    if d['GPS车速'][i] >= 10 and d['GPS车速'][i + 1] < 10:
+        # end of a fragment
+        spl.append(i)
+    i += 1
+
+print(spl)
+label = 0
+for k in range(len(spl)):
+    new = d[label:spl[k] + 1]
+    new = new.reset_index(drop=True)  # 重新进行index生成
+    # new.to_csv('processed_data/file1_fragments/fragment' + str(k+1) + '.csv')
+    # new.to_csv('processed_data/file2_fragments/fragment' + str(k+1) + '.csv')
+    new.to_csv('processed_data/file3_fragments/fragment' + str(k+1) + '.csv')
+    label = spl[k] + 1
